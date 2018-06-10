ace01e5dcaf79f5f203ec350255c7305afd7d132
stigie <stiggie@info.sgu.ru>
2018-06-10 17:53:42 +0400

    add binding input and title

--- src/App.js
+++ src/App.js
@@ -11,31 +11,65 @@ class Logo extends Component{
   }
 }
 class Title extends Component{
+  constructor(){
+    super();
+  }
   render(){
+    let msg = this.props.inputText;
+    if (msg ===""){
+      msg = "username"
+    }
     return(
-      <h1>Welcome on board, &#123;username&#125;!</h1>
+      <h1>Welcome on board, &#123;{msg}&#125;!</h1>
     );
   }
 }
 class Form extends Component{
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange() {
+    this.props.onUserInput(
+      this.inputText.value,
+    );
+  }
   render(){
     return(
       <form className="main">
-        <input className="input" placeholder="Username" />
+        <input 
+          className="input" 
+          placeholder="Username" 
+          value={this.props.inputText} 
+          ref={(input) => this.inputText = input}
+          onChange={this.handleChange} />
         <button className="submit-button">Submit</button>
       </form>
     );
   }
 }
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      inputText: ""
+    }
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+  handleUserInput(inputText) {
+    this.setState({
+      inputText: inputText,
+    });
+  }
   render() {
     return (
       <div className="App">
       <div className="cover">
         <Logo />
-        <Title />
+        <Title inputText={this.state.inputText} />
       </div>  
-        <Form />
+        <Form inputText={this.state.inputText}
+              onUserInput={this.handleUserInput} />
       </div>
     );
   }
